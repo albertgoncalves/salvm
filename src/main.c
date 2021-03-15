fed8dbd8f2b7002c1729c4136a52b14a29e3158c
Albert Goncalves <goncalves.albert@gmail.com>
2021-03-15 09:23:23 -0400

    temporary loop example

--- src/main.c
+++ src/main.c
@@ -9,11 +9,82 @@ i32 main(i32 n, const char** args) {
            sizeof(Header),
            sizeof(Vm));
     {
-        Vm vm = {0};
-        vm.insts[0].tag = INST_PUSH;
-        vm.insts[0].op = 123456;
-        vm.insts[1].tag = INST_HALT;
-        set_insts_to_file(&vm, 2, args[1]);
+        Vm  vm = {0};
+        u32 i = 0;
+
+        vm.insts[i].tag = INST_RSRV;
+        vm.insts[i++].op = 1;
+
+        vm.insts[i].tag = INST_PUSH;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i].tag = INST_STORE;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i].tag = INST_COPY;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i].tag = INST_PUSH;
+        vm.insts[i++].op = 5;
+
+        vm.insts[i++].tag = INST_EQ;
+
+        vm.insts[i].tag = INST_JPZ;
+        vm.insts[i++].op = 4;
+
+        vm.insts[i].tag = INST_COPY;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i++].tag = INST_PRINTI;
+
+        vm.insts[i].tag = INST_JUMP;
+        vm.insts[i++].op = 15;
+
+        vm.insts[i].tag = INST_COPY;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i].tag = INST_PUSH;
+        vm.insts[i++].op = 1;
+
+        vm.insts[i++].tag = INST_ADDI;
+
+        vm.insts[i].tag = INST_STORE;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i].tag = INST_PUSH;
+        vm.insts[i++].op = 3;
+
+        vm.insts[i].tag = INST_COPY;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i++].tag = INST_EQ;
+
+        vm.insts[i].tag = INST_JPZ;
+        vm.insts[i++].op = 4;
+
+        vm.insts[i].tag = INST_JUMP;
+        vm.insts[i++].op = 2;
+
+        vm.insts[i].tag = INST_JUMP;
+        vm.insts[i++].op = -1;
+
+        vm.insts[i].tag = INST_JUMP;
+        vm.insts[i++].op = -17;
+
+        vm.insts[i].tag = INST_COPY;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i++].tag = INST_PRINTI;
+
+        vm.insts[i].tag = INST_JUMP;
+        vm.insts[i++].op = -20;
+
+        vm.insts[i].tag = INST_STORE;
+        vm.insts[i++].op = 0;
+
+        vm.insts[i++].tag = INST_HALT;
+
+        set_insts_to_file(&vm, i, args[1]);
     }
     {
         Vm vm = {0};
