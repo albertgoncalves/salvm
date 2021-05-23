12325155ced16e7b200d0f47502a083cfb707109
Albert Goncalves <goncalves.albert@gmail.com>
2021-05-23 18:25:52 -0400

    enhance debug printer

--- src/vm_asm.h
+++ src/vm_asm.h
@@ -1,7 +1,7 @@
 #ifndef __VM_ASM_H__
 #define __VM_ASM_H__
 
-#include "vm.h"
+#include "vm_inst_string.h"
 
 #define CAP_CHARS  (2 << 11)
 #define CAP_TOKENS (2 << 7)
@@ -17,11 +17,6 @@ typedef enum {
     TOKEN_COLON,
     TOKEN_MINUS,
 } TokenTag;
-
-typedef struct {
-    const char* chars;
-    u32         len;
-} String;
 
 typedef union {
     String  as_string;
@@ -59,18 +54,6 @@ typedef struct {
     u32     len_labels;
 } Memory;
 
-#define TO_STRING(literal)          \
-    ((String){                      \
-        .chars = literal,           \
-        .len = sizeof(literal) - 1, \
-    })
-
-#define EQ_STRINGS(a, b) \
-    ((a.len == b.len) && (!memcmp(a.chars, b.chars, a.len)))
-
-#define PRINTLN_STR(stream, string) \
-    fprintf(stream, "`%.*s`\n", (i32)string.len, string.chars)
-
 #define IS_ALPHA(x) \
     ((('A' <= (x)) && ((x) <= 'Z')) || (('a' <= (x)) && ((x) <= 'z')))
 
@@ -96,144 +79,6 @@ ALLOC_MEMORY(alloc_label, CAP_LABELS, labels, Label)
         println_token(stderr, token); \
         ERROR();                      \
     }
-
-static String get_inst_tag_as_string(InstTag tag) {
-    switch (tag) {
-    case INST_HALT: {
-        return TO_STRING("halt");
-    }
-
-    case INST_PUSH: {
-        return TO_STRING("push");
-    }
-
-    case INST_TOP: {
-        return TO_STRING("top");
-    }
-
-    case INST_COPY: {
-        return TO_STRING("copy");
-    }
-    case INST_PUT: {
-        return TO_STRING("put");
-    }
-
-    case INST_CALL: {
-        return TO_STRING("call");
-    }
-    case INST_SCL: {
-        return TO_STRING("scl");
-    }
-    case INST_RET: {
-        return TO_STRING("ret");
-    }
-
-    case INST_BASE: {
-        return TO_STRING("base");
-    }
-    case INST_FRAME: {
-        return TO_STRING("frame");
-    }
-    case INST_RESET: {
-        return TO_STRING("reset");
-    }
-
-    case INST_RD8: {
-        return TO_STRING("rd8");
-    }
-    case INST_RD16: {
-        return TO_STRING("rd16");
-    }
-    case INST_RD32: {
-        return TO_STRING("rd32");
-    }
-
-    case INST_SV8: {
-        return TO_STRING("sv8");
-    }
-    case INST_SV16: {
-        return TO_STRING("sv16");
-    }
-    case INST_SV32: {
-        return TO_STRING("sv32");
-    }
-
-    case INST_JPZ: {
-        return TO_STRING("jpz");
-    }
-    case INST_JUMP: {
-        return TO_STRING("jump");
-    }
-
-    case INST_NOT: {
-        return TO_STRING("not");
-    }
-    case INST_EQ: {
-        return TO_STRING("eq");
-    }
-
-    case INST_ADDI: {
-        return TO_STRING("addi");
-    }
-    case INST_SUBI: {
-        return TO_STRING("subi");
-    }
-    case INST_MULI: {
-        return TO_STRING("muli");
-    }
-    case INST_DIVI: {
-        return TO_STRING("divi");
-    }
-
-    case INST_ADDF: {
-        return TO_STRING("addf");
-    }
-    case INST_SUBF: {
-        return TO_STRING("subf");
-    }
-    case INST_MULF: {
-        return TO_STRING("mulf");
-    }
-    case INST_DIVF: {
-        return TO_STRING("divf");
-    }
-
-    case INST_LTI: {
-        return TO_STRING("lti");
-    }
-    case INST_LEI: {
-        return TO_STRING("lei");
-    }
-    case INST_GTI: {
-        return TO_STRING("gti");
-    }
-    case INST_GEI: {
-        return TO_STRING("gei");
-    }
-
-    case INST_LTF: {
-        return TO_STRING("ltf");
-    }
-    case INST_LEF: {
-        return TO_STRING("lef");
-    }
-    case INST_GTF: {
-        return TO_STRING("gtf");
-    }
-    case INST_GEF: {
-        return TO_STRING("gef");
-    }
-
-    case INST_NATIVE: {
-        return TO_STRING("native");
-    }
-
-    case COUNT_INST_TAG:
-    default: {
-        ERROR();
-    }
-    }
-}
 
 static void println_token(File* stream, Token token) {
     fprintf(stream, "%u:", token.line);
--- src/vm_io.h
+++ src/vm_io.h
@@ -3,6 +3,10 @@
 
 #include "vm_inst.h"
 
+#ifdef DEBUG_PRINT_VM
+    #include "vm_inst_string.h"
+#endif
+
 static void run(Vm* vm) {
     vm->index.inst = 0;
     vm->index.stack_top = 0;
@@ -11,14 +15,21 @@ static void run(Vm* vm) {
     while (vm->alive) {
 #ifdef DEBUG_PRINT_VM
         {
+            String inst =
+                get_inst_tag_as_string(vm->insts[vm->index.inst].tag);
             printf("\n\n"
                    "    | .index.inst       : %d\n"
                    "    |       .stack_top  : %d\n"
                    "    |       .stack_base : %d\n"
+                   "    | .inst.tag         : " FMT_STR "\n"
+                   "    | .inst.op          : %d\n"
                    "    | .stack            : [ ",
                    vm->index.inst,
                    vm->index.stack_top,
-                   vm->index.stack_base);
+                   vm->index.stack_base,
+                   inst.len,
+                   inst.chars,
+                   vm->insts[vm->index.inst].op);
             for (i32 i = vm->index.stack_base; i < vm->index.stack_top; ++i) {
                 printf("%d ", vm->stack[i].as_i32);
             }
