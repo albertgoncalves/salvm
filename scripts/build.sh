bd5b8d8229a5f9bc17673b89f626519215ead1b9
albertgoncalves <goncalves.albert@gmail.com>
2021-05-27 19:50:11 -0400

    compile c-code with clang++

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,11 +28,34 @@ flags=(
     "-Wno-padded"
     "-Wno-reserved-id-macro"
 )
+flags_cpp=(
+    "-ferror-limit=1"
+    "-ffast-math"
+    "-fno-exceptions"
+    "-fno-math-errno"
+    "-fno-rtti"
+    "-fno-unwind-tables"
+    "-fshort-enums"
+    "-g"
+    "-march=native"
+    "-O3"
+    "-Werror"
+    "-Weverything"
+    "-Wno-c++98-compat-pedantic"
+    "-Wno-c11-extensions"
+    "-Wno-c99-designator"
+    "-Wno-cast-align"
+    "-Wno-covered-switch-default"
+    "-Wno-deprecated"
+    "-Wno-extra-semi-stmt"
+    "-Wno-old-style-cast"
+    "-Wno-padded"
+    "-Wno-reserved-id-macro"
+)
 bins=(
     test_vm_asm
     test_vm_inst
     vm_asm
-    vm
 )
 
 (
@@ -42,6 +65,7 @@ bins=(
         clang "${flags[@]}" -o "$WD/bin/$x" "$WD/src/$x.c"
     done
     clang "${flags[@]}" "-DDEBUG_PRINT_VM" -o "$WD/bin/vm_debug" "$WD/src/vm.c"
+    clang++ "${flags_cpp[@]}" -o "$WD/bin/vm" "$WD/src/vm.c"
     end=$(now)
     python3 -c "print(\"Compiled! ({:.3f}s)\".format(${end} - ${start}))"
 )
